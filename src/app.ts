ac743f8f6c1d9f0f33be1cafec7613eb67f5935e
Felipe Fonseca <felipeaf196@gmail.com>
2026-02-09 11:57:23 -0300

    feat: database connection setup

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config'
+import { pool } from './db/pool'
 
 import express from 'express'
 
@@ -6,8 +7,12 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.json('xdd')
+app.get('/', async (req, res) => {
+    const query = `SELECT * FROM users`
+
+    const users = await pool.query(query)
+
+    res.json(users.rows)
 })
 
 export default app
